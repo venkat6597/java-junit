494abefd3c99061c758e6b5a3fc46338a11b9cd9
venkat6597 <vengadesh6597@gmail.com>
2022-05-13 00:21:22 +0530

    Changes

--- src/main/java/com/marksandspencer/foodshub/pal/repository/PALRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marksandspencer.foodshub.pal.repository;
-
-import com.marksandspencer.foodshub.pal.domain.PALRole;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PALRoleRepository extends MongoRepository<PALRole, String> {
-}
--- src/main/java/com/marksandspencer/foodshub/pal/service/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.marksandspencer.foodshub.pal.service;
-
-import com.marksandspencer.foodshub.pal.constant.MessageTemplate;
-import com.marksandspencer.foodshub.pal.domain.PALProduct;
-import com.marksandspencer.foodshub.pal.domain.PALProject;
-
-public interface NotificationService {
-    void sendEmailMessage(PALProject savedPalProject, PALProduct palProduct, MessageTemplate messageTemplate);
-}
